55acd25a6b9ecc67df29d13243dc5f068950bd57
Xue Gong <43104091+OpalGX@users.noreply.github.com>
2021-10-12 15:35:43 +0800

    add code for linear model

---
+++ linear_hypergraph_embedding.m
@@ -0,0 +1,13 @@
+% linear_hypergraph_embedding  Embed nodes in hypergraph to a linear
+% position
+% structure
+%
+% INPUTS
+% 
+% - A2   Adjacency matrix
+% - A3   3rd order adjacency tensor
+% - gamma   Decay parameter for the linear hypergraph model
+%
+% OUTPUTS
+% - x       Node embedding
+
