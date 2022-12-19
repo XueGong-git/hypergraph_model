3ea976b22562a3593866f7d2e8c657eb883b0102
Xue Gong <xuegong@outlook.com>
2022-12-19 16:00:04 +0800

    Run other train ratio

--- prediction_high_school.m
+++ prediction_high_school.m
@@ -7,11 +7,11 @@ addpath 'functions';
 set(groot,'defaultFigureVisible','off') % 'on' to turn back on.  
 tic
 gamma_array = 1:50:500;
-c3_array = 1:1:10;
+c3_array = 0:1:10;
 n_eig = 3;
-for train_ratio = [0.8]
+for train_ratio = [0.2, 0.6, 0.8]
     for data_name = "contact-high-school"
-        PredictTriangles(data_name, train_ratio, c3_array, gamma_array, "true", n_eig)      
+        PredictTriangles(data_name, train_ratio, c3_array, gamma_array, "false", n_eig)      
     end
 end
 toc
